3b0b24400c4b95c4a8d633c2236ddb66e1007c4d
1895271 <1895271@E21015.CEGEPGIM.LCL>
2019-02-09 10:11:01 -0500

    added getters and setters in class Customer

--- src/javaapplication2/Customer.java
+++ src/javaapplication2/Customer.java
@@ -4,6 +4,22 @@ public class Customer {
     String name;
     String location;
     
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    public String getLocation() {
+        return location;
+    }
+    
     private void sendOrder() {
         System.out.println("This method calls the contents of sendOrder() method");
     }
