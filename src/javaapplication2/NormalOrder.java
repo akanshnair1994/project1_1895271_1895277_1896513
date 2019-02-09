e1cca97c19d74a223481791b9eacf152dd22a549
1896513 <1896513@E21018.CEGEPGIM.LCL>
2019-02-09 10:17:40 -0500

    Creation of NormalOrder Class with constructors setter and getters and methods

---
+++ src/javaapplication2/NormalOrder.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 1896513
+ */
+public class NormalOrder {
+
+    String number;
+    Date date;
+
+    public NormalOrder(String customerName, Date date) {
+        this.number = customerName;
+        this.date = date;
+    }
+
+    public void setdate(Date date) {
+        this.date = date;
+    }
+
+    public void setNumber(Date date) {
+        this.date = date;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public Date getdate() {
+        return date;
+    }
+
+    public void confirm() {
+        System.out.println("Creation of confirm method");
+    }
+
+    public void close() {
+        System.out.println("Creation of close method");
+    }
+
+    public void dispatch() {
+        System.out.println("Creation of dispatch method");
+    }
+
+    public void recieve() {
+        System.out.println("Creation of recieve method");
+    }
+}
